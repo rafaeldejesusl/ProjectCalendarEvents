e8c7f55f0c3281e7075ce74e7966969111892a28
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2022-08-24 19:17:30 -0300

    Testes do requisito 2

--- src/calendar-events.Test/TestReq2.cs
+++ src/calendar-events.Test/TestReq2.cs
@@ -8,17 +8,27 @@ namespace calendar_events.Test;
 public class TestReq2
 {
     [Theory(DisplayName = "Deve procurar um evento por titulo")]
-    [InlineData()]
+    [InlineData("Churrasco", "2021-04-22", "Churrasco da firma", 0)]
     public void TestListSearchByTitle(string title, string date, string description,int expected)
     {
-        throw new NotImplementedException();
+        Event eventInstance = new (title, date, description);
+        EventList instance = new ();
+        instance.GenericList();
+        instance.Add(eventInstance);
+        int result = instance.SearchByTitle(title);
+        result.Should().Be(expected);
     }
 
     [Theory(DisplayName = "Deve procurar um evento por data")]
-    [InlineData()]
+    [InlineData("Churrasco", "2021-04-22", "Churrasco da firma", 0)]
     public void TestListSearchByDate(string title, string date, string description, int expected)
     {
-        throw new NotImplementedException();
+        Event eventInstance = new (title, date, description);
+        EventList instance = new ();
+        instance.GenericList();
+        instance.Add(eventInstance);
+        int result = instance.SearchByDate(date);
+        result.Should().Be(expected);
     }
 
     
