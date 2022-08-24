2eb3704bea4490ff981a90181fa2bdc0e93c07d9
rafaeldejesusl <rafaeldejesuspp@gmail.com>
2022-08-24 19:14:42 -0300

    Testes do requisito 1

--- src/calendar-events.Test/TestReq1.cs
+++ src/calendar-events.Test/TestReq1.cs
@@ -8,32 +8,39 @@ namespace calendar_events.Test;
 public class TestReq1
 {
     [Theory(DisplayName = "Deve cadastrar um evento com o construtor completo")]
-    [InlineData()]
+    [InlineData("Churrasco", "2021-04-22", "Churrasco da firma")]
     public void TestEventFullConstructor(string title, string date, string description)
     {
-        throw new NotImplementedException();
+        Event instance = new (title, date, description);
+        instance.Should().BeAssignableTo<Event>();
     }
 
     [Theory(DisplayName = "Deve cadastrar um evento com o construtor sem descrição")]
-    [InlineData()]
+    [InlineData("Churrasco", "2021-04-22")]
     public void TestEventHalfConstructor(string title, string date)
     {
-        throw new NotImplementedException();   
+        Event instance = new (title, date);
+        instance.Should().BeAssignableTo<Event>(); 
     }
 
     [Theory(DisplayName = "Deve atrasar a data de um evento corretamente")]
-    [InlineData()]
+    [InlineData("Churrasco", "2021-04-22", 5, "2021-04-27")]
     public void TestEventDelayDate(string title, string date, int days, string expected)
     {
-        throw new NotImplementedException();
-        
+        Event instance = new (title, date);
+        var dateArray = expected.Split('-');
+        var expectedDate = new DateTime(Convert.ToInt32(dateArray[0]), Convert.ToInt32(dateArray[1]), Convert.ToInt32(dateArray[2])); 
+        instance.DelayDate(days);
+        instance.EventDate.Should().Be(expectedDate);
     }
 
     [Theory(DisplayName = "Deve imprimir um evento corretamente")]
-    [InlineData()]    
+    [InlineData("Churrasco", "2021-04-22", "Churrasco da firma", "detailed", "Evento = Churrasco\nDate = 22/04/2021\nDescription = Churrasco da firma")]    
     public void TestPrintEvent(string title, string date, string description, string format, string expected)
     {
-        throw new NotImplementedException(); 
+        Event instance = new (title, date, description);
+        var result = instance.PrintEvent(format); 
+        result.Should().Be(expected);
     }
     
     
